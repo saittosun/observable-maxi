ff9e589fa2e039f028fcd09debc263c5c2b55d45
tostos <saittosun2@gmail.com>
2020-05-12 15:21:08 +0200

    the core of observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
 
   constructor() { }
 
+  // tslint:disable-next-line:max-line-length
+  // we can now use our subscription and call unsubscribe and that means that whenever we leave that component, we clear that subscription and therefore we prevent memory leaks because we're not keeping old subscriptions around.
   ngOnInit() {
-    interval(1000).subscribe(count => {
-      console.log(count)
-    })
+    this.firstObsSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    });
+  }
+
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
   }
 
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,6 +12,8 @@ export class UserComponent implements OnInit {
   constructor(private route: ActivatedRoute) {
   }
 
+  // tslint:disable-next-line:max-line-length
+  // One common question I get is OK, we unsubscribe here, why don't we unsubscribe in the user component, here where we also set up a subscription? And that is a valid question. The answer simply is Angular does that for you. For the observables provided by Angular, like params but also any other observable you'll encounter throughout this course that is provided by an Angular package or by a feature provided by Angular, all these Angular observables are managed by Angular and therefore, you don't need to unsubscribe manually here, Angular will take care about this.
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
